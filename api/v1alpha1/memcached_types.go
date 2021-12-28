2922360a56d55e4aa3f59adb2de7e5aa9d2e4612
Guilherme Malfatti <guimalfatti76@gmail.com>
2021-12-27 21:48:51 -0500

    Create CRD and RBAC

--- api/v1alpha1/memcached_types.go
+++ api/v1alpha1/memcached_types.go
@@ -29,8 +29,7 @@ type MemcachedSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of Memcached. Edit memcached_types.go to remove/update
-	Foo  string `json:"foo,omitempty"`
-	Size int32  `json:"size"`
+	Size int32 `json:"size"`
 }
 
 // MemcachedStatus defines the observed state of Memcached
